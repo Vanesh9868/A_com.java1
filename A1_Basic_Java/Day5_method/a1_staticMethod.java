3e0392af544a81822c01969679d4ba70cd634be5
yogesh shinde <yogeshshinde9868@gmail.com>
2023-02-26 17:51:45 +0530

    created 1 method in master

--- A1_Basic_Java/Day5_method/a1_staticMethod.java
+++ A1_Basic_Java/Day5_method/a1_staticMethod.java
@@ -17,7 +17,10 @@ public class a1_staticMethod {
 		System.out.println("fly in sky");
 	}
 
-	
+	public static void m1 () 
+	{
+		System.out.println("m1");
+	}
 	public static void main(String[] args) { 
 	
 		    a1_staticMethod.cat  ();               //class name.method name();
