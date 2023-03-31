808bfac35d2e0742c953617b9fa634962dc6369c
yogesh shinde <yogesh.shinde@docquity.com>
2023-03-31 10:21:18 +0530

    set application script added

--- A1_Basic_Java/Dayz12_String_methods/o6_split_trim.java
+++ A1_Basic_Java/Dayz12_String_methods/o6_split_trim.java
@@ -3,11 +3,10 @@ package Dayz12_String_methods;
 public class o6_split_trim {
 
 	public static void main(String[] args) {
-
-
 		String product="Tomato - 1 Kg";
-		System.out.println(product);
 		String productR=product.split("-")[0].trim();
+		
+		System.out.println(product);
 		System.out.println(productR);
 	}
 
