a1e41d980ca1e5f01ed0e142b4efa52e3ded314b
yogesh shinde <yogesh.shinde@docquity.com>
2023-04-12 13:09:53 +0530

    jump_continue

--- A4_prcatice/page1/p1.java
+++ A4_prcatice/page1/p1.java
@@ -1,11 +1,27 @@
 package page1;
 
+import java.util.Arrays;
+
 public class p1 {
 
 	public static void main(String[] args) {
 
 
-		System.out.println("p1");
-	}
+		String x = "the test";
+		char s[]=x.toCharArray();
+		
+		int Tcount=0;
+		
+		for(int i=0; i<x.length(); i++) {
+			
+			if(s[i]== 't') {
+				System.out.println(s[i]);
+				Tcount=Tcount+1;
+			}
+			
+		}
+		
+		System.out.println(Tcount);
 
+	}
 }
